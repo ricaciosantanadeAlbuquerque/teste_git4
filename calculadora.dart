2c037e4eaa711929164879da45c0a5a9dcb21c8a
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-10-06 02:26:24 -0300

    Classe calculadora

--- calculadora.dart
+++ calculadora.dart
@@ -1,1 +1,11 @@
-class Calculadora{}
+import 'agregacao.dart';
+import 'composicao.dart';
+
+class Calculadora {
+  
+  final Agregacao agregado;
+  final Composicao componente;
+
+  const Calculadora({required this.agregado, required this.componente});
+
+}
